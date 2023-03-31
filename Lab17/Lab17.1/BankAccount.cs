21946a219a42b150f8001722667fb6bec4aabc5d
utkinDE <utkinde@gmail.com>
2023-03-31 16:03:54 +0700

    .

---
+++ Lab17/Lab17.1/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17._1
+{
+    class BankAccount<T>
+    {
+        private T AccountNumber { get; set; }
+        private double Balance { get; set; }
+        private string Name { get; set; }
+
+        public BankAccount(T accountNumber, double balance, string name)
+        {
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+            this.Name = name;
+        }
+
+        public string GetInfo()
+        {
+            return $"{AccountNumber} {Balance} {Name}";
+        }
+    }
+}
---
+++ Lab17/Lab17.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строковый номер счёта в банке");
+            string accountString = Console.ReadLine();
+            Console.WriteLine("Введите сумму");
+            double balanceDouble = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string nameString = Console.ReadLine();
+
+            BankAccount<string> bankAccount1 = new BankAccount<string>(accountString, balanceDouble, nameString);
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            Console.WriteLine("Введите числовой номер счёта в банке");
+            int accountInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сумму");
+            balanceDouble = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            nameString = Console.ReadLine();
+
+            BankAccount<int> bankAccount2 = new BankAccount<int>(accountInt, balanceDouble, nameString);
+            Console.WriteLine(bankAccount2.GetInfo());
+        }
+    }
+}
